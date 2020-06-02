1230d052c034f2f599326226e40dfde4b0c9b06e
Kalleu Oliveira <kalleu.oliveira@apiki.com>
2020-06-01 22:54:59 -0300

    feat: add structure reactjs

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', () => {
+app.get('/users', (req, res) => {
   console.log('Listagem de usuários');
+
+  res.json([
+    'Davi',
+    'Lidy',
+    'Edy'
+  ]);
 });
+
+app.listen(3333);
