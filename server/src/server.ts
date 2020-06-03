33c11bb2f9914360a25648d3b0460543f7f2a226
Kalleu Oliveira <kalleu.oliveira@apiki.com>
2020-06-02 23:08:40 -0300

    feat: add server aplication

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,15 @@
+import path from 'path';
+
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  console.log('Listagem de usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  res.json([
-    'Davi',
-    'Lidy',
-    'Edy'
-  ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
